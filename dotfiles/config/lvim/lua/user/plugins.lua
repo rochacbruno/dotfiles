faeacbe2a88ad82ce844dd715ad75cd1298dc43b
Bruno Rocha <rochacbruno@gmail.com>
2022-12-23 12:51:36 +0000

    Update dotfiles

--- dotfiles/config/lvim/lua/user/plugins.lua
+++ dotfiles/config/lvim/lua/user/plugins.lua
@@ -260,5 +260,22 @@ lvim.plugins = {
         yank_dry_run = true,
       })
     end
-  }
+  },
+  -- Surrounds
+  --    Old text                    Command         New text
+  --------------------------------------------------------------------------------
+  -- surr*ound_words             ysiw)           (surround_words)
+  -- *make strings               ys$"            "make strings"
+  -- [delete ar*ound me!]        ds]             delete around me!
+  -- remove <b>HTML t*ags</b>    dst             remove HTML tags
+  -- 'change quot*es'            cs'"            "change quotes"
+  -- <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+  -- delete(functi*on calls)     dsf             function calls
+  {
+    "kylechui/nvim-surround",
+    config = function()
+      require("nvim-surround").setup()
+    end
+  },
 }
+
